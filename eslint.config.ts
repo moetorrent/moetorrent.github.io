857e0694cfe8e0879d6caceb345e6a6a1eb3e83a
stabldev <114811070+stabldev@users.noreply.github.com>
2026-06-16 14:01:15 +0530

    chore: cleanup boilerplate and change metadata

--- eslint.config.ts
+++ eslint.config.ts
@@ -5,7 +5,10 @@ import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   eslintConfigPrettier,
