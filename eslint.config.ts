d6b4e06c8fb5030b9ed87d9fa861f32e7d2022a0
stabldev <114811070+stabldev@users.noreply.github.com>
2026-06-16 13:50:04 +0530

    fix: re-run neatlint to patch eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,9 +2,11 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]);
