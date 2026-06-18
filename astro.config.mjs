ed11aae7b363ff38916144c447f0dcfb8613471d
stabldev <114811070+stabldev@users.noreply.github.com>
2026-06-18 14:29:39 +0530

    feat: astro index page with .mdx extension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  devToolbar: {
+    enabled: false,
+  },
+
+  integrations: [mdx()],
 });
