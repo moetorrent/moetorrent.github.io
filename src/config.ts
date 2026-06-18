64ff1957a9a2dc88624be425bd5cf41131672d07
stabldev <114811070+stabldev@users.noreply.github.com>
2026-06-18 16:00:07 +0530

    feat: add simple download section in index page

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const APP_VERSION = "0.1.2";
+
+export const DOWNLOAD_URLS = {
+  windowsNsis: `https://github.com/moetorrent/moetorrent/releases/download/v${APP_VERSION}/moeTorrent_${APP_VERSION}_x64-setup.exe`,
+};
